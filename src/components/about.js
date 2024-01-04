e8eb8a5f19ada750dc236f00aa3ac380db4ae20d
Furqan Ahmed Khan <furqan.ahmedkhan1998@gmail.com>
2024-01-03 23:43:11 -0800

    latest new update

--- src/components/about.js
+++ src/components/about.js
@@ -5,13 +5,11 @@ const AboutUs = () => {
     <div className="bg-black py-12" id="about">
       <div className="container mx-auto text-center">
         <h1 className="text-4xl md:text-6xl font-bold text-yellow-600">
-          Your Recovery Team
+        Your Recovery Team
         </h1>
         <p className="text-yellow-900 text-base md:text-xl lg:text-xl mt-4">
-          Our experience and proven track record make us well-equipped to manage
-          your recovery case. Our success can be attributed to our dedicated
-          team, which includes licensed investigators, research analysts,
-          mobile notaries, and a qualified attorney.
+          With a wealth of expertise and a demonstrated history of success, we are adept at overseeing your recovery case. Our achievements stem from the commitment of our skilled team, comprising licensed investigators, research analysts, mobile notaries, and a qualified attorney.
+        
         </p>
       </div>
     </div>
--- src/components/features.js
+++ src/components/features.js
@@ -1,19 +1,12 @@
 
-
-
-
-
-
-
-
-
-
 import React from "react";
 import {
   PencilIcon,
   CashIcon,
-  DocumentReportIcon,
+  DocumentIcon,
   TranslateIcon,
+  PencilAltIcon,
+  SearchIcon,
 } from "@heroicons/react/solid";
 import FeaturesBg from "../assets/featuresbg.jpg";
 
@@ -31,22 +24,24 @@ const Features = () => {
       icon: (
         <CashIcon className="w-16 h-16 text-yellow-600 mx-auto border border-white rounded-full p-3" />
       ),
-      title: "$440,108",
-      content: "Recovered in our largest net to client",
+      title: "No Out-Of-Pocket Costs",
+      content: "At Legacy Rising, we are committed to delivering our services without imposing any financial strain on our clients. To uphold this commitment, we do not require any upfront fees. Our operating model is built on a contingency basis, where our payment is contingent upon successfully recovering funds on your behalf. This ensures a mutual alignment of interests, assuring you that we will exert every effort to optimize your claim with diligence and trustworthiness. And should any Legal fees arise, those too will be taken care of by Legacy Rising and our licensed legal team.",
     },
     {
       icon: (
-        <DocumentReportIcon className="w-16 h-16 text-yellow-600 mx-auto border border-white rounded-full p-3" />
+        <SearchIcon className="w-16 h-16 text-yellow-600 mx-auto border border-white rounded-full p-3" />
+        // <DocumentIcon className="w-16 h-16 text-yellow-600 mx-auto border border-white rounded-full p-3" />
       ),
-      title: "Record Results",
-      content: "Weekly case reviews with the State ensure claims move forward",
+      title: "Devoted Research Specialists and Private Investigators ",
+      content: " At Legacy Rising, our success is driven by a committed team of research specialists and private investigators. Their expertise is instrumental in identifying individuals potentially entitled to unclaimed funds. Upon identification we engage with these individuals, outlining our process and furnishing the essential information for them to kickstart their claims. ",
     },
     {
       icon: (
-        <TranslateIcon className="w-16 h-16 text-yellow-600 mx-auto border border-white rounded-full p-3" />
+        // <TranslateIcon className="w-16 h-16 text-yellow-600 mx-auto border border-white rounded-full p-3" />
+        <PencilAltIcon className="w-16 h-16 text-yellow-600 mx-auto border border-white rounded-full p-3" />
       ),
-      title: "Bilingual",
-      content: "Contact to inquire",
+      title: "Claim Retrieval",
+      content: "We hold a license and collaborate directly with the county to aid clients in submitting claims. Our responsibilities include conducting audits on county records, and they furnish us with information and regular updates on the status of claims, streamlining the process.",
     },
   ];
 
